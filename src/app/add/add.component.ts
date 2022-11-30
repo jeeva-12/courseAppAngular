b32f45c854cd571de827e4d76e51fcc16219af4d
jeeva-12 <112372759+jeeva-12@users.noreply.github.com>
2022-11-30 09:25:08 +0530

    fwf

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add',
@@ -12,13 +13,20 @@ export class AddComponent {
   courseDate=""
   courseVenue=""
 
-
+constructor(private api:ApiService){}
 readValue=()=>
 {
 let data:any=
 {
   "courseTitle":this.courseTitle,"courseDescription":this.courseDescription,"courseDuration":this.courseDescription,"courseDate":this.courseDate, "courseVenue":this.courseVenue}
 console.log(data)
+
+this.api.addCourse(data).subscribe(
+  (response)=>
+  {
+    console.log(response)
+  }
+)
 }
 
 
---
+++ src/app/api.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  fetchCourse = () =>
+  {
+    return this.http.get("https://mylinkurcodesapp.herokuapp.com/getcourses")
+  }
+  addCourse =(dataToSend:any) =>
+  {
+    return this.http.post("https://mylinkurcodesapp.herokuapp.com/addcourse",dataToSend)
+  }
+}
